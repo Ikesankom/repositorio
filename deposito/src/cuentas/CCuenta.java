2f20d455e90c76f4f1a7ba3e767984a512175cdf
hugo <hugo_gongora@dam.entornos.com>
2024-03-03 19:36:45 +0100

    6 Insertar comentarios JavaDoc

--- deposito/src/cuentas/CCuenta.java
+++ deposito/src/cuentas/CCuenta.java
@@ -1,4 +1,10 @@
 package cuentas;
+/**
+ * @autor Hugo José Góngora Lozano
+ * 
+ * @version v0.1 
+ * @since 03/03/2024
+ */
 public class CCuenta {
 
 
@@ -6,41 +12,56 @@ public class CCuenta {
     private String cuenta;
     private double saldo;
     private double tipoInterés;
+    /**@param nombre. Es el nombre del cliente.
+     * @param cuenta. Número de cuenta.
+     * @param saldo. Cantidad de dinero en cuenta (euros).
+     * @param tipoInterés. Tipo de interés aplicado en la cuenta
+     * */
 
-	public String getNombre() {
+	public String getNombre() {  
 		return nombre;
+		/**@return Devuelve nombre del cliente*/
 	}
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
+		/**@param nombre. Establece nombre de cliente*/
 	}
 
 	public String getCuenta() {
 		return cuenta;
+		/**@return Devuelve número de cuenta*/
 	}
 
 	public void setCuenta(String cuenta) {
 		this.cuenta = cuenta;
+		/**@param cuenta. Establece número de cuenta*/
 	}
 
 	public double getSaldo() {
 		return saldo;
+		/**@return Devuelve saldo en cuenta*/
 	}
 
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
+		/**@param saldo. Establece saldo de cuenta*/
 	}
 
 	public double getTipoInterés() {
 		return tipoInterés;
+		/**@return Devuelve tipo de interés de cuenta*/
 	}
 
 	public void setTipoInterés(double tipoInterés) {
 		this.tipoInterés = tipoInterés;
+		/**@param tipoInterés. Establece saldo de cuenta
+		 * NO SE ACONSEJA USAR NOMBRES CON TILDES*/
 	}
 
 	public CCuenta()
     {
+		/**@return Constructor vacío*/
     }
 
     public CCuenta(String nom, String cue, double sal, double tipo)
@@ -48,11 +69,14 @@ public class CCuenta {
         nombre =nom;
         cuenta=cue;
         saldo=sal;
+        /**@return Constructor con parámetros*/
     }
 
     public double estado()
     {
         return saldo;
+        /**@return Devuelve saldo de cuenta. Duplicado con método getSaldo
+         * @see getSaldo */
     }
 
     public void ingresar(double cantidad) throws Exception
@@ -60,6 +84,8 @@ public class CCuenta {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         saldo = saldo + cantidad;
+        /**@return suma cantidad al saldo
+         * @throws arroja un error si la cantidad es negativa */ 
     }
 
     public void retirar(double cantidad) throws Exception
@@ -69,5 +95,8 @@ public class CCuenta {
         if (estado()< cantidad)
             throw new Exception ("No se hay suficiente saldo");
         saldo = saldo - cantidad;
+        /**@return resta cantidad al saldo
+         * @throws arroja un error si la cantidad es negativa 
+         * o si se intenta retirar más saldo del disponible */ 
     }
 }
